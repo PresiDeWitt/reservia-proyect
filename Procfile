11f2af595999c260d52447b98e6042bb354f435c
PresiDeWitt <alexchaconortega@gmail.com>
2026-03-11 20:56:55 +0100

    Clean project: remove unused files, add .gitignore

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: python backend/manage.py migrate && gunicorn -w 3 backend.reservia.wsgi:application --bind 0.0.0.0:$PORT
--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running database migrations..."
-cd backend
-python manage.py migrate --noinput || true
-echo "Migrations complete!"
